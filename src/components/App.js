b156007034e2989b8c6e78a553670258495a6f32
Rafal Kochanecki <rafal.kochanecki@gmail.com>
2023-11-21 20:24:48 +0100

    task 3 done

--- src/components/App.js
+++ src/components/App.js
@@ -8,23 +8,37 @@ import Task03 from './../../03/Task03';
 import Task04 from './../../04/Task04';
 import Task05 from './../../05/Task05';
 
-const task2Store = createStore(() => {
-    return {
-        message: 'Działa!',
+const initState = { 
+    message: 'Działa!',
+    time: new Date()
+};
+
+const reducer = (state = initState, action) => {
+    switch(action.type) {
+        case 'getCurrentTime':
+            return {
+                ...state,
+                time: new Date(),
+            }
+        default:
+            return state;
     }
-});
+}
+
+const store = createStore(
+    reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 const App = () => {
     return (
-        <>
+        <Provider store={store}>
             {/* <Task01 /> */}
-            <Provider store={task2Store}>
-                <Task02 />
-            </Provider>
-            {/* <Task03 /> */}
+            {/* <Task02 /> */}
+            <Task03 />
             {/* <Task04 /> */}
             {/* <Task05 /> */}
-        </>
+        </Provider>
     )
 }
 
